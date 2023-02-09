4c392067744311747fee4ccd230df2eb05841c0c
Stefano Bozzi <bozzistef@gmail.com>
2023-02-09 20:19:07 -0300

    feat: add header

---
+++ src/components/layout/TheHeader.tsx
@@ -0,0 +1,11 @@
+import "./TheHeader.css";
+
+export default function TheHeader() {
+  return (
+    <header className="header">
+      <div className="container">
+        <h1 className="header__logo">Hacker news</h1>
+      </div>
+    </header>
+  );
+}
