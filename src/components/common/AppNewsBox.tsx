0d25b0ac2b42ba88d4908f4165ad1ca2f5a64463
Stefano Bozzi <bozzistef@gmail.com>
2023-02-09 20:35:07 -0300

    feat: create news box

---
+++ src/components/common/AppNewsBox.tsx
@@ -0,0 +1,23 @@
+import { Icon } from "@iconify/react";
+
+import "./AppNewsBox.css";
+
+export default function AppNewsBox() {
+  return (
+    <div className="news-box">
+      <div className="news-box__body">
+        <p className="news-box__date">
+          <Icon icon="ic:baseline-access-time" color="#606060" width="16" height="16" />
+          <span className="news-box__time">3 hours ago by author</span>
+        </p>
+        <p className="news-box__title">Yes, React is taking over front-end development. The question is why.</p>
+      </div>
+      <div className="news-box__action">
+        <button type="button" className="news-box__button">
+          {/* <Icon icon="ion:heart" color="#dd0031" width="24" height="22" /> */}
+          <Icon icon="mdi:cards-heart-outline" color="#dd0031" width="24" height="22" />
+        </button>
+      </div>
+    </div>
+  );
+}
