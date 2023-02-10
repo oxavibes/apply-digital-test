85a65d2b51c5821da89daef4042e4f958e45d586
Stefano Bozzi <bozzistef@gmail.com>
2023-02-10 00:46:31 -0300

    fix: hide default option in select

--- src/pages/NewsPage.tsx
+++ src/pages/NewsPage.tsx
@@ -26,7 +26,9 @@ export default function NewsPage() {
         <>
           <section className="news-selector">
             <select className="news-select">
-              <option value="">Select your news</option>
+              <option value="" selected disabled hidden>
+                Select your news
+              </option>
               <option value="angular"> Angular</option>
               <option value="react">React</option>
               <option value="vue">Vue</option>
